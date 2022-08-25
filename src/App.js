aa14ef7db832bb2770eb32549d1f7e47312f2493
Ivan <example@gmail.com>
2022-08-25 14:28:23 +0300

    created reducers and made some refact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body w-full ">
+      <div className="container">
+        <div className="app-wrapper">
+          <Header />
+          <div className="main">
+            <Navbar />
+            <div className="content borderDef border-blue-400 ">
+              <Routes>
+                <Route
+                  path="/dialogs/*"
+                  element={
+                    <Dialogs
+                      dialogsPage={props.state.dialogsPage}
+                      dispatch={props.dispatch}
+                    />
+                  }
+                />
+
+                <Route
+                  path="/profile/*"
+                  element={
+                    <Profile
+                      profilePage={props.state.profilePage}
+                      dispatch={props.dispatch}
+                    />
+                  }
+                />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/state";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let renderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App
+          state={state}
+          dispatch={store.dispatch.bind(store)}
+          store={store}
+        />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+store.subscribe(renderEntireTree);
+renderEntireTree(store.getState());
